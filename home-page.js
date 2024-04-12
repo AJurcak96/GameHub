20ad8785369d09314f3338f8b88d46899a121b99
Aleksandar Jurcak <aleksandar.jurcak996@gmail.com>
2024-04-12 17:49:03 +0200

    Edited NavBar, added some horizontal lines and animation for Welcome section

--- home-page.js
+++ home-page.js
@@ -1,24 +1,60 @@
-document.querySelector(".menu-toggle").addEventListener("click", function () {
-  this.classList.toggle("active");
-});
-
 // Select the menu toggle element
 const menuToggle = document.querySelector(".menu-toggle");
 
 // Select the buttons inside the header nav link
 const headerNavButtons = document.querySelectorAll(".header-nav-link button");
 
-// Function to handle clicks anywhere else on the screen
+// Select the input field
+const inputField = document.querySelector(".input-field");
+
+// Function to handle clicks anywhere else on the document
 function handleClickOutside(event) {
-  // Check if the clicked element is not the menu toggle, its child, or one of the buttons
+  // Check if the clicked element is not the menu toggle, its child, one of the buttons, or the input field
   if (
     !menuToggle.contains(event.target) &&
-    ![...headerNavButtons].includes(event.target)
+    ![...headerNavButtons, inputField].includes(event.target)
   ) {
-    // Remove the 'active' class from the menu toggle
+    // Collapse the menu by removing the 'active' class from the menu toggle
     menuToggle.classList.remove("active");
   }
 }
 
-// Add click event listener to the document body
+// Function to collapse the menu when the screen is resized
+function handleResize() {
+  // Check if the menu toggle has the 'active' class
+  if (menuToggle.classList.contains("active")) {
+    // Collapse the menu by removing the 'active' class from the menu toggle
+    menuToggle.classList.remove("active");
+  }
+}
+
+// Add click event listener to the document body to handle clicks anywhere else on the document
 document.body.addEventListener("click", handleClickOutside);
+
+// Add click event listener to the menu toggle to toggle its 'active' class
+menuToggle.addEventListener("click", function () {
+  this.classList.toggle("active");
+});
+
+// Add resize event listener to the window object to handle screen resizing
+window.addEventListener("resize", handleResize);
+
+// Letters displaying function
+document.addEventListener("DOMContentLoaded", function () {
+  function showTextLetterByLetter(elementId, text, speed) {
+    const element = document.getElementById(elementId);
+    element.textContent = "";
+
+    let i = 0;
+    const intervalId = setInterval(function () {
+      element.textContent += text[i++];
+      if (i === text.length) clearInterval(intervalId);
+    }, speed);
+  }
+
+  showTextLetterByLetter(
+    "subText",
+    "Sign up or Login and start a discussion",
+    100
+  );
+});
